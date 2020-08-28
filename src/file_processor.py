779726dad35b538c68c101b9d96a613e08c42bed
Tamara Bartlett <tamara.bartlett@uplight.com>
2020-08-28 12:40:44 -0600

    Can merge lists

--- src/file_processor.py
+++ src/file_processor.py
@@ -1,6 +1,43 @@
-import os
-import sys
-from pymemcache.client.base import Client
+import collections
+import functools
+import string
+import operator
 
-def read_file(file):
-    return file
+def get_dict_of_words(line):
+    line_arr = str.split(line, ' ')
+    dict_of_words = dict()
+    for word in line_arr:
+        normalized_word = get_normalized_word(word)
+        if normalized_word not in dict_of_words.keys():
+            dict_of_words[normalized_word] = 1
+        else:
+            dict_of_words[normalized_word] += 1
+    return dict_of_words
+
+
+def get_normalized_word(word):
+    lower_word = word.lower()
+    no_punct = str.maketrans('', '', string.punctuation)
+    lower_word_no_punct = lower_word.translate(no_punct)
+    return lower_word_no_punct
+
+
+def merge_dicts(dict_1, dict_2):
+    list_dicts = [dict_1, dict_2]
+    return functools.reduce(operator.add,
+         map(collections.Counter, list_dicts))
+
+
+def read_file(input):
+    input_lines = str.splitlines(input)
+    num_input_lines = input_lines[0]
+    text_lines = []
+
+    for i in range(int(num_input_lines)+1):
+        text_lines.append(input_lines[i+1])
+    return text_lines
+
+
+if __name__ == '__main__':
+    nput_string = 'Wait a minute. Wait A minute, Doc'
+    get_dict_of_words(nput_string)
--- tests/test_file_processor.py
+++ tests/test_file_processor.py
@@ -1,17 +1,39 @@
 from unittest import TestCase
 from src.file_processor import (
+    get_dict_of_words,
+    get_normalized_word,
+    merge_dicts,
     read_file
 )
 
 class FileProcessorTests(TestCase):
     def test_file_returns_self(self):
         input_file = '''2
-        Wait a minute. Wait a minute, Doc.
-        Are you telling me that you built a time machine out of a DeLorean?'''
+Wait a minute. Wait a minute, Doc.
+Are you telling me that you built
+a time machine out of a DeLorean?'''
 
-        output_file = read_file(input_file)
+        output_array = read_file(input_file)
+        expected_array = ['Wait a minute. Wait a minute, Doc.', 'Are you telling me that you built', 'a time machine out of a DeLorean?']
 
-        self.assertEqual(input_file, output_file)
+        self.assertEqual(len(output_array), 3)
+        self.assertEqual(output_array, expected_array)
+
+    def test_merge_dicts(self):
+        dict_1 = {'wait':2, 'a': 2, 'minute': 2, 'doc': 1}
+        dict_2 = {'are':2, 'you': 2, 'telling': 2, 'doc': 3, 'a': 2}
+        expected_dict = {'wait':2 , 'a': 4, 'minute': 2, 'doc': 4, 'are':2, 'you': 2, 'telling': 2}
+        merged_dict = merge_dicts(dict_1, dict_2)
+        self.assertEqual(expected_dict, merged_dict)
+
+    def test_words_into_dict(self):
+        input_string = 'Wait a minute. Wait A minute, Doc'
+        expected_dict = {'wait':2, 'a': 2, 'minute': 2, 'doc': 1}
+        output = get_dict_of_words(input_string)
+        self.assertEqual(expected_dict, output)
+
+    def test_get_plain_word(self):
+        self.assertEqual(get_normalized_word('Word.'), 'word')
 
 if __name__ == '__main__':
     unittest.main()
